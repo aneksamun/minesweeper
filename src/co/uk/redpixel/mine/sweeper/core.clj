f02c0c3d211d6d3386fc5031946f48186339e94b
Sergey Terentyev <sergey.terentyev@kindredgroup.com>
2019-02-07 13:06:14 +0000

    Initial

---
+++ src/co/uk/redpixel/mine/sweeper/core.clj
@@ -0,0 +1,32 @@
+(ns co.uk.redpixel.mine.sweeper.core)
+
+(def neighbours
+  [[-1, 1][0, 1][1, 1]
+   [-1, 0]      [1, 0]
+   [-1,-1][0,-1][1,-1]])
+
+(defn neighbours-of [x, y]
+  (set (map (fn [[x-offs y-offs]] [(+ x-offs x) (+ y-offs y)]) neighbours)))
+
+(defn generate-cell [neighbours x y]
+  (if (contains? neighbours [x y]) 1 0))
+
+(defn generate-line [neighbours width y]
+  ((map (partial generate-cell neighbours y)
+        (range 0 width))))
+
+(defn generate-board [dimensions neighbours]
+  (mapcat (partial generate-line neighbours (dimensions :w))
+          (range 0 (dimensions :h))))
+
+(defn mine? [cell]
+  (= \* cell))
+
+(defn board-for-cell [dimensions y x cell]
+  (generate-board dimensions (if (mine? cell) (neighbours-of x y))))
+
+(defn board-for-line [dimensions line y]
+  (map (partial board-for-cell dimensions y)
+       (range 0 (dimensions :w))
+       line))
+
