7e281970258ae96da0f372e0d98b59d62026d297
Sergey Terentyev <sergejs.terentjevs@gmail.com>
2019-02-10 00:30:02 +0000

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mine-sweeper "0.1.0-SNAPSHOT"
+(defproject minesweeper "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
