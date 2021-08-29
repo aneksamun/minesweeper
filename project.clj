96fb71dfe17744bf76d1fc1e7173ad4051a5d869
Sergey Terentyev <sergejs.terentjevs@gmail.com>
2021-08-29 02:46:18 +0100

    Upgraded Clojure version

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/aneksamun/minesweeper"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]]
   :aot [co.uk.redpixel.minesweeper.application]
   :main co.uk.redpixel.minesweeper.application)
